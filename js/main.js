2510e1166c2a4d5e1fcabfeced62aee2d5be5a58
Candimeponemucho <pedrinmetin@gmail.com>
2024-11-12 15:57:55 +0100

    1

--- js/main.js
+++ js/main.js
@@ -10,3 +10,24 @@ function toggleDescription(index) {
         description.style.display = "none"; // Oculta la descripción
     }
 }
+
+
+function validatePassword() {
+    // Obtener el valor ingresado en el campo de contraseña
+    const password = document.getElementById('password').value;
+    
+    // Verificar si la contraseña es correcta
+    if (password === 'OHANA') {
+        // Si la contraseña es correcta, redirigir a index2.html
+        window.location.href = 'index2.html';
+        return false; // Evita el envío del formulario
+    } else {
+        // Si la contraseña es incorrecta, mostrar el mensaje de error y recargar la página
+        const errorMessage = document.getElementById('error-message');
+        errorMessage.style.display = 'block'; // Mostrar el mensaje de error
+        setTimeout(function() {
+            window.location.reload(); // Recargar la página después de unos segundos
+        }, 2000); // 2 segundos de espera antes de recargar
+        return false; // Evita el envío del formulario
+    }
+}
