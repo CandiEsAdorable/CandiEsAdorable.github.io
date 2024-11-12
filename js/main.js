271874d437fb16204a8c2416fd80792e7b8307b1
Candimeponemucho <pedrinmetin@gmail.com>
2024-11-12 16:04:33 +0100

    1

--- js/main.js
+++ js/main.js
@@ -12,7 +12,10 @@ function toggleDescription(index) {
 }
 
 
-function validatePassword() {
+function validatePassword(event) {
+    // Prevenir que el formulario se envíe
+    event.preventDefault();
+
     // Obtener el valor ingresado en el campo de contraseña
     const password = document.getElementById('password').value;
     
@@ -20,7 +23,6 @@ function validatePassword() {
     if (password === 'OHANA') {
         // Si la contraseña es correcta, redirigir a index2.html
         window.location.href = 'index2.html';
-        return false; // Evita el envío del formulario
     } else {
         // Si la contraseña es incorrecta, mostrar el mensaje de error y recargar la página
         const errorMessage = document.getElementById('error-message');
@@ -28,6 +30,5 @@ function validatePassword() {
         setTimeout(function() {
             window.location.reload(); // Recargar la página después de unos segundos
         }, 2000); // 2 segundos de espera antes de recargar
-        return false; // Evita el envío del formulario
     }
 }
