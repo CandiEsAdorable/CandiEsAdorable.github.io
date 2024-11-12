c5f78c8c047a0e7a5544c389ac2264add45a8ca5
Candimeponemucho <pedrinmetin@gmail.com>
2024-11-12 14:35:39 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
+// Esta función se ejecutará cuando el usuario haga clic en el botón
 function toggleDescription(index) {
+    // Obtener el elemento de la descripción basado en el ID
     var description = document.getElementById("description-" + index);
+
+    // Alternar la visibilidad del elemento
     if (description.style.display === "none" || description.style.display === "") {
         description.style.display = "block"; // Muestra la descripción
     } else {
